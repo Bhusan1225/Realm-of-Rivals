e5ed1eb35fa9236b2f0bae1fcc477e664caf0a54
Bhusan <bhusan12sahoo@gmail.com>
2024-12-03 08:52:45 +0530

    Zharith the Narrator
    Zharith the Narrator of the game is introduced.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Zharith
+{
+public:
+	Zharith() 
+	{
+		cout << "Hi, I am Zharith the game narrator " << endl;
+	}
+	
+}; 
+
+
+int main() 
+{
+	Zharith* zhar = new Zharith();
+	
+	
+	
+	
+	return 0;
+
+}
